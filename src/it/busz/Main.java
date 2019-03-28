c1deb43534beb762631290944778ff889a7953c8
Michal Busz <michal@busz.it>
2019-03-28 19:12:01 +0000

    done

--- src/it/busz/Main.java
+++ src/it/busz/Main.java
@@ -1,8 +1,23 @@
 package it.busz;
 
-public class Main {
+import java.math.BigInteger;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        for (;num>0;num--){
+            int number = input.nextInt();
+            System.out.println(factorial(BigInteger.valueOf(number)).toString());
+        }
+    }
+
+    private static BigInteger factorial(BigInteger number){
+        if(number.equals(BigInteger.valueOf(1)))
+            return number;
+        else
+            return number.multiply(factorial(number.subtract(BigInteger.valueOf(1))));
     }
 }
+
